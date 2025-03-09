32a44ec0bfcd120fb26ff17ba18becc1b7585f8c
LukaCek <cek.luka10@gmail.com>
2025-03-09 16:31:59 +0000

    push rewrite, adding docs

--- example.py
+++ example.py
@@ -3,16 +3,10 @@ import time
 
 printer = printer(
     ip="192.168.1.244",
-    printer_id="01P00A392200553",
-    password="24661618"
+    printer_id="01P00A000000000",
+    password="12341234"
 )
 printer.debug = True
-printer.home()
 
-while True:
-    i = int(input("colling fan speed: "))
-    printer.cooling_fan(i)
-    i = int(input("chamber fan speed: "))
-    printer.chamber_fan(i)
-    i = int(input("aux fan speed: "))
-    printer.aux_fan(i)
+printer.start_print("Rose.gcode.3mf", use_ams=True, timelapse=True)
+printer.push(under_temperature=26, object_hight=41.3)
